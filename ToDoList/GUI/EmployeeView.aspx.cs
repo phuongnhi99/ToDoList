0773c4c101c3b07c27933ae7a21b1042dff7ea2e
๖HERO乛Sóiツ <loveyou010199@gmail.com>
2020-09-24 21:13:15 +0700

    update Employee

--- ToDoList/GUI/EmployeeView.aspx.cs
+++ ToDoList/GUI/EmployeeView.aspx.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ToDoList.BLL;
 
 namespace ToDoList
 {
     public partial class Employee : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        public void Page_Load(object sender, EventArgs e)
         {
+            List<Entity.Employee> emp = EmployeeBLL.GetAll();
+            tblEmployee.DataSource = emp.ToList();
+            tblEmployee.DataBind();
+        }
 
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Entity.Employee employee = new Entity.Employee()
+            {
+                ID = txtID.Text,
+                Email = txtEmail.Text,
+                PassWord = txtPassWord.Text,
+                Name = txtName.Text,
+                DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text),
+                PhoneNumber = txtPhoneNumber.Text,
+                Position = txtPosition.Text,
+                Level = txtLevel.Text
+            };
+            EmployeeBLL.AddEmp(employee);
+            Page_Load(sender, e);
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            Entity.Employee employee = new Entity.Employee()
+            {
+                ID = txtID.Text,
+                Email = txtEmail.Text,
+                PassWord = txtPassWord.Text,
+                Name = txtName.Text,
+                DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text),
+                PhoneNumber = txtPhoneNumber.Text,
+                Position = txtPosition.Text,
+                Level = txtLevel.Text
+            };
+            EmployeeBLL.updateEmp(employee);
+            Page_Load(sender, e);
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            Entity.Employee employee = new Entity.Employee()
+            {
+                ID = txtID.Text,               
+            };
+            EmployeeBLL.delete(employee);
+            Page_Load(sender, e);
         }
     }
 }
