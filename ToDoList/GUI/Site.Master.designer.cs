1ad15045a97e049efc12fb80e9cce236e40c62d7
phuong-mai <55681843+phuong-mai@users.noreply.github.com>
2020-09-27 18:02:34 +0700

    fix

--- ToDoList/GUI/Site.Master.designer.cs
+++ ToDoList/GUI/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace ToDoList.GUI
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
