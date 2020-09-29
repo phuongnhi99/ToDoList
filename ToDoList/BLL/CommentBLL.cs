cb309dfb8508a4d0f70325772f92c5f0077b6317
phuong-mai <55681843+phuong-mai@users.noreply.github.com>
2020-09-30 00:31:34 +0700

    commit ngày 30/9

---
+++ ToDoList/BLL/CommentBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToDoList.DAL;
+using ToDoList.Entity;
+
+namespace ToDoList.BLL
+{
+    public static class CommentBLL
+    {
+        private static CommentDAL obj = new CommentDAL();
+        public static void AddComment(Comment comment)
+        {
+            obj.AddComment(comment);
+        }
+
+        public static void updateComment(Comment comment)
+        {
+            obj.updateComment(comment);
+        }
+        public static void delete(Comment comment)
+        {
+            obj.Delete(comment);
+        }
+    }
+}
